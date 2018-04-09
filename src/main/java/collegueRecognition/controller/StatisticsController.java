777050cbdbd5e6ecff16b944e5523bace752dab8
biso <biserivanov08@gmail.com>
2018-04-09 13:20:23 +0300

    Added statistics page with dummy functionality.

--- src/main/java/collegueRecognition/controller/StatisticsController.java
+++ src/main/java/collegueRecognition/controller/StatisticsController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 @Controller
 public class StatisticsController {
@@ -15,6 +16,22 @@ public class StatisticsController {
         User user = (User) request.getSession().getAttribute("loggedUser");
         if(user != null) {
             model.addAttribute("loggedUser", user);
+            ArrayList<String> names = new ArrayList<>();
+            names.add("biso");
+            names.add("ivan");
+            names.add("martin");
+            names.add("dragan");
+            names.add("petkan");
+            names.add("niki");
+            ArrayList<Integer> votes = new ArrayList<>();
+            votes.add(12);
+            votes.add(10);
+            votes.add(7);
+            votes.add(7);
+            votes.add(4);
+            votes.add(1);
+            model.addAttribute("names", names);
+            model.addAttribute("votes", votes);
             return "statistics";
         }
         return "login";
