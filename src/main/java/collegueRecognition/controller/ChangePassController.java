cb7ae7af07173d06d816211a38bb3dd35844d389
biso <biserivanov08@gmail.com>
2019-08-04 12:25:48 +0300

    added change pass frontend

---
+++ src/main/java/collegueRecognition/controller/ChangePassController.java
@@ -0,0 +1,22 @@
+package collegueRecognition.controller;
+
+import collegueRecognition.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ChangePassController {
+
+    @GetMapping(value = {"/changePass"})
+    public String changePass(HttpServletRequest request, Model model) {
+        User user = (User) request.getSession().getAttribute("loggedUser");
+        if(user != null) {
+            model.addAttribute("loggedUser", user);
+            return "changePass";
+        }
+        return "login";
+    }
+}
